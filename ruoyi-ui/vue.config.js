c41b56369e63c8e00b3a65081fe38fdd56479682
xiaoruibin <>
2020-07-15 09:07:39 +0800

    update 连接地址

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: `http://116.196.76.171:18000`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
